5ad3d08d3a5fc452700f8b9763e4fca28a80ecb0
Mr.be0001 <bittuthakur22221@gmail.com>
2025-08-21 00:01:52 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -203,8 +203,9 @@ app.post('/login', async (req, res) => {
     });
   }
 });
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:5000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 process.on('unhandledRejection', (reason) => {
   console.error('Unhandled Promise Rejection:', reason);
